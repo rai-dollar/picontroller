799e9249c9caf62219b49d08d98fc2e621d560bf
Bert Kellerman <5810624+bkellerman@users.noreply.github.com>
2025-04-17 15:05:06 -0400

    Revert "Freeze and get updaters (#1)" (#2)
    This reverts commit becd58c516a9f9f407078045d6d9bdf9f7952b49.

--- scripts/publish.py
+++
@@ -1,7 +0,0 @@
-from ape import networks
-
-def main():
-    with networks.ethereum.sepolia.use_provider("infura"):
-        networks.provider.network.explorer.publish_contract("0x05d57a15102bF7E80081321F9F5d7B8b390E887b")
-
-
